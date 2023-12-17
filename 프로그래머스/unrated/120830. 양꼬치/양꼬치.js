cfba1ef3d6effdfc726857c7847056c137e66dd8
석호 <126848879+SeokHoChoi@users.noreply.github.com>
2023-12-18 01:47:03 +0900

    [level unrated] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,15 @@
+/**
+* @param {number} n - 양꼬치 먹은 인분
+* @param {number} k - 음료수 마신 수
+* @return 지불할 가격
+*
+* @description
+* 할인받을 수 있는 음료의 개수를 얻은 후 그만큼을 제하고 가격을 책정합니다
+*/
+function solution(n, k) {
+    const offsetOfN = 12000 * 10;
+    const offsetKOfK = parseInt((n * 12000) / offsetOfN, 10);
+    const price = (n * 12000) + (k - offsetKOfK) * 2000;
+
+    return price;
+}
