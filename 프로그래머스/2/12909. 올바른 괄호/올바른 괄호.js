a2ad8f480bfe67e7042c3f78b69b40dd15c6075a
석호 <126848879+SeokHoChoi@users.noreply.github.com>
2024-02-14 16:45:03 +0900

    [level 2] Title: 올바른 괄호, Time: 4.37 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,43 +1,19 @@
 function solution(s){
-    var answer = true;
     const stack = [];
-    let i = 0;
+    
     for (let i = 0; i < s.length; i++) {
-        const tmp = s[i];
-        if (tmp === '(') {
-            stack.push(tmp);
-        } else {
-           const tmp2 = stack.pop();
-            if (tmp2 === ')' || tmp2 === undefined) {
-                return false;
-              
-            } else {
-                answer = true;
-            }
+        const char = s[i];
+        
+        if (char === '(') {
+            stack.push(char);
+            continue;
         }
-    }
-    if (!!stack.length) {
-        answer = false;
+        
+        if (stack.length === 0) {
+            return false;
+        }
+        stack.pop();
     }
     
-
-    return answer;
+    return stack.length === 0;
 }
-
-// function solution(s) {
-//     const stack = [];
-    
-//     for (let i =  0; i < s.length; i++) {
-//         const char = s[i];
-//         if (char === '(') {
-//             stack.push(char);
-//         } else if (char === ')') {
-//             if (stack.length ===  0) {
-//                 return false; 
-//             }
-//             stack.pop();
-//         }
-//     }
-    
-//     return stack.length ===  0;
-// }
