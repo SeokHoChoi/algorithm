cefa8a2f6a89d0a7af3b2e8b0388b4225dc9a2ea
석호 <126848879+SeokHoChoi@users.noreply.github.com>
2024-02-19 15:14:00 +0900

    [level 2] Title: 전화번호 목록, Time: 115.37 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -2,7 +2,7 @@ function solution(phone_book) {
     phone_book.sort();
 
     for (let i =  0; i < phone_book.length -  1; i++) {
-        if (phone_book[i +  1].indexOf(phone_book[i]) ===  0) {
+        if (phone_book[i + 1].indexOf(phone_book[i]) ===  0) {
             return false;
         }
     }
