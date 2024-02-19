4fc6cef8e47ed7c7eeae27154683f8abd409a1de
석호 <126848879+SeokHoChoi@users.noreply.github.com>
2024-02-19 16:05:32 +0900

    [level 2] Title: 전화번호 목록, Time: 98.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,23 @@
-function solution(phone_book) {
+function solution(phone_book) {    
+// 시간 초과
+/*
+    var answer = true;
+    for (let i = 0; i < phone_book.length; i++) {
+        for (let j = 0; j < phone_book.length; j++) {
+            const number = phone_book[i];
+            const len = number.length;
+            if (i !== j && number === phone_book[j].slice(0, len)) {
+                return false;
+            }
+        }
+    }
+    
+    return answer;
+*/
+    
+// 시간 초과 해결
+/*
     phone_book.sort();
-
     for (let i =  0; i < phone_book.length -  1; i++) {
         if (phone_book[i + 1].indexOf(phone_book[i]) ===  0) {
             return false;
@@ -8,18 +25,17 @@ function solution(phone_book) {
     }
 
     return true;
+*/
     
-// 시간 초과
-//     var answer = true;
-//     for (let i = 0; i < phone_book.length; i++) {
-//         for (let j = 0; j < phone_book.length; j++) {
-//             const number = phone_book[i];
-//             const len = number.length;
-//             if (i !== j && number === phone_book[j].slice(0, len)) {
-//                 return false;
-//             }
-//         }
-//     }
-    
-//     return answer;
+// 시간 초과 해결
+
+    phone_book.sort();
+    for (let i =  0; i < phone_book.length -  1; i++) {
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
+        }
+    }
+
+    return true;
+
 }
