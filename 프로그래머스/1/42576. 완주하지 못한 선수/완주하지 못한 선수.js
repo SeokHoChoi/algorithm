0522842f1f80c36c86560cd10321bcf31fd96d39
석호 <126848879+SeokHoChoi@users.noreply.github.com>
2024-02-19 14:21:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 28.73 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,26 @@
+function solution(participant, completion) {
+    const map = new Map();
+
+       participant.forEach((name) => {
+        if (!map.has(name)) {
+            map.set(name,  1);
+        } else {
+            map.set(name, map.get(name) +  1);
+        }
+    });
+
+    completion.forEach((name) => {
+        if (map.has(name)) {
+            map.set(name, map.get(name) -  1);
+        }
+    });
+
+    for (const [name, count] of map) {
+        if (count >  0) {
+            return name;
+        }
+    }
+    
+    
+    return null
+}
